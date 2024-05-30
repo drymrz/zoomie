38d7100b47a639d0f374897b0af14ec177b0b7a7
Adry Mirza <adrymirza@gmail.com>
2024-05-30 11:22:30 +0700

    feat: finish personal meeting room todo: create own css style

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -7,12 +7,14 @@ import { useGetCalls } from '@/hooks/useGetCalls';
 import MeetingCard from './MeetingCard';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useToast } from './ui/use-toast';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
     const router = useRouter();
     const { endedCalls, upcomingCalls, callRecordings, isLoading } =
         useGetCalls();
     const [recordings, setRecordings] = useState<CallRecording[]>([]);
+    const { toast } = useToast();
 
     const getCalls = () => {
         switch (type) {
@@ -42,15 +44,19 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 
     useEffect(() => {
         const fetchRecordings = async () => {
-            const callData = await Promise.all(
-                callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
-            );
+            try {
+                const callData = await Promise.all(
+                    callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
+                );
 
-            const recordings = callData
-                .filter((call) => call.recordings.length > 0)
-                .flatMap((call) => call.recordings);
+                const recordings = callData
+                    .filter((call) => call.recordings.length > 0)
+                    .flatMap((call) => call.recordings);
 
-            setRecordings(recordings);
+                setRecordings(recordings);
+            } catch (error) {
+                toast({ title: 'Failed to fetch recordings, Try again later' })
+            }
         };
 
         if (type === 'recordings') {
@@ -79,7 +85,7 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
                         title={
                             (meeting as Call).state?.custom?.description ||
                             (meeting as CallRecording).filename?.substring(0, 20) ||
-                            'No Description'
+                            'Personal Meeting'
                         }
                         date={
                             (meeting as Call).state?.startsAt?.toLocaleString() ||
